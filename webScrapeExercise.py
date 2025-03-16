6762c433ff9b41e61732141c5fd8099224293f8b
joanne.seymour <1619878217@qq.com>
2025-03-17 07:03:54 +0800

    exercise 1, finished returning first substring name

--- webScrapeExercise.py
+++ webScrapeExercise.py
@@ -2,3 +2,12 @@
 
 import re
 from urllib.request import urlopen
+page = urlopen("http://olympus.realpython.org/profiles/dionysus")
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+print(html)
+string_to_find = "Name"
+string_start_index = html.find(string_to_find)
+string_end_index = string_start_index + len(string_to_find)
+print(f"{string_to_find} starts at {string_start_index} and ends at {string_end_index}")
+print(html[string_start_index:string_end_index])
